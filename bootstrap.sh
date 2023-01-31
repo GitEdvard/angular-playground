253ed543114df398259a6001be680285e521f7be
Edvard Englund <edvard.englund@medsci.uu.se>
2023-01-31 10:49:30 +0100

    Fix angular language server

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
 # I chose yes for increased typecheck
 # say no to routing
 # choose css to stylesheet option
-ng new ng-fundamentals
+npm install -g @angular/cli@11.2.3
 nvm install 14.16.0
 nvm use 14.16.0
-npm install -g @angular/cli@11.2.3
+# ng new ng-fundamentals
+npm install # creates node_modules 
+# npm start
