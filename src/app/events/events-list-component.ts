0ac39bb87fb561ab7d6881d6ec43b66f93b9a065
Edvard Englund <edvard.englund@medsci.uu.se>
2023-02-02 08:32:39 +0100

    Communicating with child components

---
+++ src/app/events/events-list-component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core'
+
+@Component ({
+  selector: 'events-list',
+  template: `
+    <div>
+      <h1>Upcoming Angular Events</h1>
+      <hr>
+      <event-thumbnail [event]="event1"></event-thumbnail>
+    </div>
+  `
+})
+export class EventsListComponent {
+  event1 = {
+    id: 1,
+    name: 'Angular connect',
+    date: '230131',
+    time: '14:30:00',
+    price: 10,
+    imageUrl: '/assets/images/angularconnectl-shield.png',
+    location: {
+      address: '1035 DT',
+      city: 'London',
+      country: 'England',
+    }
+  }
+}
